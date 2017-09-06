4f90e04a616d5d6ef533191c194b299498e29e6d
杉山航 <cjgjc803@gmail.com>
2017-09-06 15:06:31 +0900

    Initial Commit

---
+++ Quadratic equation_not_Solution formula/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Quadratic equation_not_Solution formula
+//
+//  Created by 杉山航 on 2017/09/06.
+//  Copyright © 2017年 杉山航. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
